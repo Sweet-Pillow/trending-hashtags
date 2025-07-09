cc50e8758a3710bb52208e9db0ef7b36fe6d393e
Sweet-Pillow <daniel_h.m@outlook.com>
2025-07-09 15:31:01 -0300

    Defining the input class

---
+++ script.js
@@ -0,0 +1,7 @@
+const INPUT_CLASS = "notranslate public-DraftEditor-content"
+
+document.addEventListener("click", function () {
+  if (document.activeElement.className === INPUT_CLASS) {
+    
+  }
+})
